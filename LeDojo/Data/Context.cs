da1d46b31927ad60f2032be78362ed8d150a3d5b
Maxime <maxime.bouchouareb@outlook.com>
2021-12-22 10:55:10 +0100

    First functional commit with first migration

--- LeDojo/Data/Context.cs
+++ LeDojo/Data/Context.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.EntityFramework;
+using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
@@ -6,6 +7,7 @@ using System.Web;
 
 namespace LeDojo.Data
 {
+    [DbConfigurationType(typeof(MySqlEFConfiguration))]
     public class Context : DbContext
     {
         // You can add custom code to this file. Changes will not be overwritten.
